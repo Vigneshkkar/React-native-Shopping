dc03c26e007b9424559fcade1fe00bdf542620d3
Vigneshkkar <vigneshkkar@Vigneshkkars-MacBook-Pro.local>
2020-09-20 14:53:47 +0530

    Checking

--- App.js
+++ App.js
@@ -24,7 +24,7 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React = () => {
+const App: () => React$Node = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
